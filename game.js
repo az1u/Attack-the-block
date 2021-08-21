3bb55d4cfc17c653dcf03a835d6811b53700aedb
az1u <ziglar.5@gmail.com>
2021-08-21 11:36:20 -0400

    first commit, character movement code complete

---
+++ game.js
@@ -0,0 +1,28 @@
+//creating a asset to move around
+let mover = document.querySelector('.mover')
+let modifier = 10
+
+window.addEventListener('load', () =>{
+mover.style.top = 0
+mover.style.left = 0
+mover.style.position = 'absolute'
+
+})
+
+window.addEventListener('keyup' , (e) =>{
+  
+    switch(e.key){
+        case 'ArrowUp': 
+            mover.style.top = parseInt(mover.style.top) - modifier + 'px'; 
+            break
+        case 'ArrowDown': 
+            mover.style.top = parseInt(mover.style.top) + modifier + 'px'; 
+            break
+        case 'ArrowRight': 
+            mover.style.left = parseInt(mover.style.left) + modifier + 'px'; 
+            break
+        case 'ArrowLeft': 
+            mover.style.left = parseInt(mover.style.left) - modifier + 'px'; 
+            break
+    }
+})
