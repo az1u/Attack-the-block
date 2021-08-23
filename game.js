df56611dc22dcde454b9075773c695d64da5f1be
az1u <ziglar.5@gmail.com>
2021-08-23 08:41:25 -0400

    game backdrop and obsatcle framework

--- game.js
+++ game.js
@@ -1,3 +1,77 @@
+var randomNumberBetween = (min, max) => {
+    return min + Math.random()*(max-min)
+}
+
+class Vector{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+    add(vector){
+        this.x += vector.x
+        this.y += vector.y
+    }
+    static random(minX,maxX, minY, maxY){
+        return new Vector( 
+            randomNumberBetween(minX,maxX),
+            randomNumberBetween(minY,maxY)
+        )
+    }
+}
+class Barrier{
+    constructor(width, height){
+        this.pos = Vector.random(0, width, 0, height)
+        this.vel= Vector.random(0,0);
+        this.acc = new Vector( 0,0)
+
+    }
+    update(){
+        this.pos.add(this.vel);
+        this.vel.add(this.acc);
+        this.acc*0;
+    }
+}
+class Canvas{
+    constructor(){
+        this.canvas = document.createElement('canvas')
+        this.ctx = this.canvas.getContext('2d')
+       
+        document.body.appendChild(this.canvas)
+
+        this.canvas.width = 1080;
+        this.canvas.height = 500;
+        this.setup();
+        requestAnimationFrame(() => this.update());
+        }
+    setup(){
+        const NUM_BARRIERS = 5;
+        this.barrier =[];
+
+        for(let i = 0; i < NUM_BARRIERS; i++){
+            this.barrier.push(new Barrier(
+            randomNumberBetween(0, this.canvas.width),
+            randomNumberBetween(0, this.canvas.height)
+            ))
+        }
+    }
+      update() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+        for(let block of this.barrier){ 
+            block.update();
+            this.ctx.fillStyle = 'rgba(255,255, 255, 1)';
+            this.ctx.beginPath();
+            this.ctx.fillRect( 10, 10, 20 , 5 );
+            this.ctx.fill();
+        }
+        requestAnimationFrame(() => this.update());
+      }
+    
+}
+
+new Canvas()
+
+
 //creating a asset to move around
 let mover = document.querySelector('.mover')
 let modifier = 10
